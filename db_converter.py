7af3d8df36bf1f6164c27b84473375d5972bca49
Dominic Hamon <dma+github@stripysock.com>
2016-06-10 11:39:53 -0700

    fix db_converter pylint warnings

--- db_converter.py
+++ db_converter.py
@@ -2,37 +2,36 @@
 import logging
 import markov
 import argparse
-import string
 import sys
 
 try:
-  import cPickle as pickle
+    import cPickle as pickle
 except ImportError:
-  import pickle
+    import pickle
 
 #
 
-argparser = argparse.ArgumentParser(description='Convert databases.')
-argparser.add_argument('--in', dest='db_in', default='./oldcharrakdb')
-argparser.add_argument('--out', dest='db_out', default='./newcharrakdb')
-args = argparser.parse_args()
+ARGPARSER = argparse.ArgumentParser(description='Convert databases.')
+ARGPARSER.add_argument('--in', dest='db_in', default='./oldcharrakdb')
+ARGPARSER.add_argument('--out', dest='db_out', default='./newcharrakdb')
+ARGS = ARGPARSER.parse_args()
 
 #
 
-db_in = str(args.db_in)
-db_out = str(args.db_out)
+DB_IN = str(ARGS.db_in)
+DB_OUT = str(ARGS.db_out)
 
 try:
-  with open(db_in, 'rb') as dbfile:
-    db = pickle.load(dbfile)
+    with open(DB_IN, 'rb') as dbfile:
+        DB = pickle.load(dbfile)
 except IOError:
-  logging.error('Unable to read database file "%s"' % db_in)
-  sys.exit(1)
+    logging.error('Unable to read database file "%s"', DB_IN)
+    sys.exit(1)
 except ValueError:
-  logging.error('Database "%s" corrup or unreadable' % db_in)
-  sys.exit(2)
+    logging.error('Database "%s" corrupt or unreadable', DB_IN)
+    sys.exit(2)
 
-mc = markov.MarkovChain(db_out)
-mc.db = db[0]
-mc.saveDatabase()
+MC = markov.MarkovChain(DB_OUT)
+MC.db = DB[0]
+MC.saveDatabase()
 
